278594f7a8f103df955542a52764ee16737457fb
danish <danishalif16@gmail.com>
2023-03-26 20:59:27 +0700

    memperbaiki MinerPatrol

--- Assets/Scripts/Miner/MinerPatrol.cs
+++ Assets/Scripts/Miner/MinerPatrol.cs
@@ -12,69 +12,61 @@ public class MinerPatrol : MonoBehaviour
     private int currentWaypointIndex = 0;
     private bool isChasing = false;
 
-    Vector3 prevLocation = Vector3.zero;
-
     // Update is called once per frame
     private void Update()
     {
-        //mendapatkan arah gerak enemy saat ini
-        Vector3 curVelocity  = (transform.position - prevLocation);
-
-        prevLocation = transform.position;
-
         if (!DetectPlayer())
-        {
-            //gerak ke arah waypoints saat ini
-            if (transform.position != waypoints[currentWaypointIndex].transform.position){
-                transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed);
-            } else {
-                //hal yg dilakukan ketika sampai ke waypoints saat ini
-                if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1f)
+        {    
+            //hal yg dilakukan ketika sampai ke waypoints saat ini
+            if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1f)
+            {
+                // 1. mendapatkan waypoints selanjutnya dan berhenti
+                currentWaypointIndex++;
+                      
+                // 2. jika sampai ke waypoints terakhir, waypoints selanjutnya adalah waypoints pertama
+                if (currentWaypointIndex >= waypoints.Length)
                 {
-                    // 1. mendapatkan waypoints selanjutnya
-                    currentWaypointIndex++;
-                                
-                    // 2. jika sampai ke waypoints terakhir, waypoints selanjutnya adalah waypoints pertama
-                    if (currentWaypointIndex >= waypoints.Length)
-                    {
-                        currentWaypointIndex = 0;
-                    }
+                    currentWaypointIndex = 0;
                 }
-            }    
-        }     
+            }
+                
+            //gerak ke arah waypoints saat ini
+            transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed);
+        }
     }
 
     private bool DetectPlayer()
     {
-        // Membuat vision penjaga
+        // Mendapatkan semua objek yang masuk ke vision
         Collider2D[] players = Physics2D.OverlapCircleAll(transform.position, vision, playerLayer);
 
         foreach (Collider2D player in players)
         {
-            // Mendeteksi player di area vision
+            // Cek apakah ada player di vision
             RaycastHit2D hit = Physics2D.Raycast(transform.position, player.transform.position - transform.position, vision, playerLayer);
             if (hit.collider != null && hit.collider.gameObject.CompareTag("Player"))
             {
-                StartCoroutine(Chase(player));
+                // mengejar ke arah player
+                StartCoroutine(Chase(player, hit));
             }
         }
 
         return isChasing;
     }
-    
-    //menggambar vision penjaga di editor
+
     void OnDrawGizmosSelected()
     {
+        // menggambar vision di editor
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, vision);
     }
 
     //membuat penjaga mengejar player selama 1 detik ketika player ada di range penjaga
-    private IEnumerator Chase(Collider2D player)
+    private IEnumerator Chase(Collider2D player, RaycastHit2D hit)
     {  
         isChasing = true;
         transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(2);
         isChasing = false;
     }
 }
