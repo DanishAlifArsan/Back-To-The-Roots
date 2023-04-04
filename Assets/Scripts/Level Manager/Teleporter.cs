9e11b5710573a80bb4f6b06e3ab7b1abf62f3d5f
danish <danishalif16@gmail.com>
2023-04-04 18:55:50 +0700

    membuat tutorial

---
+++ Assets/Scripts/Level Manager/Teleporter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleporter : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col) {
+        if (col.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
